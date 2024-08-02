1abdcf75a8c8c379b895d5786f45ffcd95b17123
DESKTOP-0IAT0DD\Dharam <dhirpstar@gmail.com>
2024-08-02 11:17:31 +0530

    core files

---
+++ contact.php
@@ -0,0 +1,255 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>BHAGWANTRAO ARTS & SCIENCE COLLEGE</title>
+<meta http-equiv="imagetoolbar" content="no">
+<link rel="stylesheet" href="index.css" type="text/css">
+
+<script type="text/javascript" src="jquery-1.7.2.min.js"></script>
+<style type="text/css">
+div#container
+{
+   width: 800px;
+   position: relative;
+   margin-top: 0px;
+   margin-left: auto;
+   margin-right: auto;
+   text-align: left;
+}
+</style>
+<style type="text/css">
+body
+{
+   text-align: center;
+   margin: 0;
+   background-color: #FFFFFF;
+   color: #000000;
+   scrollbar-face-color: #ECE9D8;
+   scrollbar-arrow-color: #000000;
+   scrollbar-3dlight-color: #ECE9D8;
+   scrollbar-darkshadow-color: #716F64;
+   scrollbar-highlight-color: #FFFFFF;
+   scrollbar-shadow-color: #ACA899;
+   scrollbar-track-color: #D4D0C8;
+}
+</style>
+<script type="text/javascript">
+<!--
+function Validatecontact(theForm)
+{
+var strFilter = /^[A-Za-z���������������������������������������������������������������������� \t\r\n\f]*$/;
+var chkVal = theForm.Editbox1.value;
+if (!strFilter.test(chkVal))
+{
+   alert("Please enter only letter and whitespace characters in the \"Name\" field.");
+   theForm.Editbox1.focus();
+   return false;
+}
+if (theForm.Editbox1.value == "")
+{
+   alert("Please enter a value for the \"Name\" field.");
+   theForm.Editbox1.focus();
+   return false;
+}
+var strFilter = /^[-+]?\d*\.?\d*$/;
+var chkVal = theForm.Editbox6.value;
+if (!strFilter.test(chkVal))
+{
+   alert("Please enter only digit characters in the \"Phone\" field.");
+   theForm.Editbox6.focus();
+   return false;
+}
+var strValue = theForm.Editbox7.value;
+var strFilter = /^([0-9a-z]([-.\w]*[0-9a-z])*@(([0-9a-z])+([-\w]*[0-9a-z])*\.)+[a-z]{2,6})$/i;
+if (!strFilter.test(strValue))
+{
+   alert("Please enter a valid email address.");
+   theForm.Editbox7.focus();
+   return false;
+}
+if (theForm.Editbox7.value == "")
+{
+   alert("Please enter a value for the \"Email\" field.");
+   theForm.Editbox7.focus();
+   return false;
+}
+if (theForm.TextArea1.value == "")
+{
+   alert("Please enter a value for the \"Message\" field.");
+   theForm.TextArea1.focus();
+   return false;
+}
+return true;
+}
+//-->
+</script>
+<script type="text/javascript">
+    var GB_ROOT_DIR = "greybox/";
+</script>
+<script type="text/javascript" src="greybox/AJS.js"></script>
+<script type="text/javascript" src="greybox/AJS_fx.js"></script>
+<script type="text/javascript" src="greybox/gb_scripts.js"></script>
+<link href="greybox/gb_styles.css" rel="stylesheet" type="text/css" media="all" />
+
+
+
+<script type="text/javascript" src="stmenu.js"></script>
+<script type="text/javascript" src="wwb6.js"></script>
+<link rel="stylesheet" href="./BASC.css" type="text/css">
+<!--[if IE]>
+<style type="text/css">
+   img { behavior: url("pngfix.htc"); }
+</style>
+<![endif]-->
+</head>
+<body>
+<div id="container">
+<div id="Layer1" style="position:absolute;background-color: #B5DFE1;left:0px;top:380px;width:800px;height:1020px;z-index:14" title="">
+<div id="wb_Text2" style="position:absolute;left:20px;top:49px;width:550px;height:992px;z-index:0;" align="justify">
+<font style="font-size:13px" color="#000000" face="Verdana">
+
+
+<div id="wb_Text1" style="position:absolute;left:25px;top:0px;width:370px;height:32px;z-index:34;" align="justify">
+<font style="font-size:13px" color="#000000" face="Verdana"><b>
+
+Bhagwantrao Arts & Science College,<br> 
+Etapalli Dist. Gadchiroli-442704<br>
+E-mail ID :- bascollegeetapalli@gmail.com<br>
+Phone: 9423122212<br>
+Phone No.07136 223159<br>
+<br>
+Bhagwantrao Memorial Shikshan Sanstha<br>
+Aheri  Dist. Gadchiroli-442705<br>
+E-mail ID :- bhagwantraosanstha@gmail.com<br>
+Phone No.07133 272030
+
+
+
+</b></font></div>
+
+<div id="wb_Text11" style="position:absolute; left:100px; top:480px; width:210px; height:14px; z-index:36;" align="left">
+<font style="font-size:12px" color="#000000" face="Verdana"><b>Fill this form for contacting us:</b></font></div>
+<div id="wb_Form1" style="position:absolute; left:55px; top:520px; width:290px; height:293px; z-index:37">
+<form name="contact" method="post" action="contact.php" enctype="multipart/form-data" id="Form1" onsubmit="return Validatecontact(this)">
+<div id="wb_Text4" style="position:absolute;left:4px;top:15px;width:58px;height:14px;z-index:13;" align="left">
+<font style="font-size:12px" color="#000000" face="Verdana">Name*</font></div>
+<input type="text" id="Editbox1" style="position:absolute;left:70px;top:15px;width:198px;height:17px;font-family:Verdana;font-size:11px;z-index:14" name="name" value="">
+<div id="wb_Text5" style="position:absolute;left:4px;top:44px;width:58px;height:14px;z-index:15;" align="left">
+<font style="font-size:12px" color="#000000" face="Verdana">Address</font></div>
+<input type="text" id="Editbox2" style="position:absolute;left:70px;top:44px;width:198px;height:17px;font-family:Verdana;font-size:11px;z-index:16" name="address" value="">
+<div id="wb_Text6" style="position:absolute;left:4px;top:73px;width:58px;height:14px;z-index:17;" align="left">
+<font style="font-size:12px" color="#000000" face="Verdana">City</font></div>
+<input type="text" id="Editbox3" style="position:absolute;left:70px;top:73px;width:198px;height:17px;font-family:Verdana;font-size:11px;z-index:18" name="city" value="">
+<div id="wb_Text7" style="position:absolute;left:4px;top:160px;width:66px;height:28px;z-index:19;" align="left">
+<font style="font-size:12px" color="#000000" face="Verdana">Message*</font></div>
+<div id="wb_Text9" style="position:absolute;left:4px;top:102px;width:58px;height:14px;z-index:20;" align="left">
+<font style="font-size:12px" color="#000000" face="Verdana">Phone</font></div>
+<input type="text" id="Editbox6" style="position:absolute;left:70px;top:102px;width:198px;height:17px;font-family:Verdana;font-size:11px;z-index:21" name="phone" value="">
+<div id="wb_Text10" style="position:absolute;left:4px;top:131px;width:58px;height:14px;z-index:22;" align="left">
+<font style="font-size:12px" color="#000000" face="Verdana">Email*</font></div>
+<input type="text" id="Editbox7" style="position:absolute;left:70px;top:131px;width:198px;height:17px;font-family:Verdana;font-size:11px;z-index:23" name="email" value="">
+<input type="submit" id="Button1" name="Button1" value="Send" style="position:absolute;left:175px;top:248px;width:96px;height:25px;background-color:#ECE9D8;font-family:Arial;font-size:13px;z-index:24">
+<input type="reset" id="Button2" name="Button2" value="Reset" style="position:absolute;left:71px;top:248px;width:96px;height:25px;background-color:#ECE9D8;font-family:Arial;font-size:13px;z-index:25">
+<textarea name="TextArea1" id="TextArea1" style="position:absolute;left:70px;top:160px;width:198px;height:77px;font-family:Verdana;font-size:11px;z-index:26" rows="4" cols="27"></textarea>
+</form>
+</div>
+<div id="wb_Extension2" style="position:absolute; left:40px; top:205px; width:325px; height:225px; z-index:38;" align="left">
+<table width="100%" border="0" cellpadding="0" cellspacing="5">
+<tr>
+<td align="left" valign="top" width="250" height="230"><table bgcolor="#000000" border="1" cellpadding="4" cellspacing="3"><tr><td bgcolor="#FFFFEE" align="center" valign="top" width="250" height="230"><a href="images/contact.jpg" rel="gb_imageset[nice_pics]" title="contact.jpg"><img src="images/contact.jpg" width="250" height="230" border="0" alt="" /></a></td></tr></table></td>
+</tr>
+</table>
+
+</div>
+
+
+
+
+</font></div>
+</div>
+<div id="wb_MasterPage3" style="position:absolute;left:0px;top:0px;width:779px;height:610px;z-index:15;" align="left">
+
+
+</div>
+<div id="wb_MasterPage1" style="position:absolute;left:0px;top:1377px;width:800px;height:39px;z-index:16;" align="left">
+<div id="wb_Shape1" style="position:absolute;left:0px;top:0px;width:800px;height:30px;z-index:6;" align="center">
+<img src="images/img0002.png" id="Shape1" align="top" alt="" title="" border="0" width="800" height="30"></div>
+<div id="wb_Text1" style="position:absolute;left:125px;top:3px;width:550px;height:20px;z-index:7;" align="center">
+<font style="font-size:16px" color="#FFFFFF" face="Lucida Sans Unicode">&#169;</font><font style="font-size:13px" color="#FFFFFF" face="Lucida Sans Unicode"> </font><font style="font-size:13px" color="#FFFFFF" face="Verdana">2010 xyz.com. Designed and developed by <a href="http://www.datalogysoftware.com" target="_blank" class="style1">Datalogy Software</a>-9823153940.</font></div>
+</div>
+
+<div id="wb_MasterPage2" style="position:absolute;left:0px;top:0px;width:800px;height:317px;z-index:17;" align="left">
+<div id="wb_Shape2" style="position:absolute;left:0px;top:0px;width:800px;height:110px;z-index:8;" align="center">
+<img src="images/img0018.png" id="Shape2" align="top" alt="" title="" border="0" width="800" height="110">
+
+<div id="wb_Shape2" style="position:absolute;left:0px;top:0px;width:100px;height:100px;z-index:8;" align="center">
+<img src="images/logo.jpg" id="Shape2" align="top" alt="" title="" border="0" width="100" height="100"> </div>
+
+</div>
+
+<div id="wb_Text1" style="position:absolute;left:75px;top:5px;width:650px;height:14px;z-index:9;" align="center">
+<font style="font-size:12px" color="#000000" face="Verdana"><b>RUN BY BHAGWANTRAO MEMORIAL SHIKSHAN SANSTHA , AHERI.</b></font></div>
+
+<div id="wb_Text2" style="position:absolute;left:20px;top:25px;width:755px;height:82px;z-index:10;" align="center">
+<font style="font-size:21px" color="#000000" face="Verdana">BHAGWANTRAO ARTS &amp; SCIENCE COLLEGE,
+<br>ETTAPALLI. DIST. GADCHIROLI - 442704<br>
+</font><font style="font-size:13px" color="#000000" face="Arial"><b>AFFILIATED TO GONDWANA UNIVERSITY, GADCHIROLI.<br>
+UGC 2(F) Accreditated by 'NAAC' with grade 'C'</b></font></div>
+
+
+
+
+
+<div id="wb_Shape1" style="position:absolute;left:0px;top:116px;width:800px;height:201px;z-index:11;" align="center">
+<img src="images/img0003.gif" id="Shape1" align="top" alt="" title="" border="0" width="800" height="201">
+
+
+
+<div id="wb_Shape1" style="position:absolute;left:0px;top:237px;width:800px;height:30px;z-index:19;" align="center">
+<img src="images/img0001.png" id="Shape1" align="top" alt="" title="" border="0" width="800" height="30">
+<div id="wb_Text1" style="position:absolute;left:34px;top:8px;width:186px;height:16px;z-index:20;" align="left">
+<font style="font-size:13px" color="#FFFFFF" face="Verdana"><b>CONTACT US</b></font></div>
+</div>
+
+
+<div id="container">
+<div class="drop">
+<div id="wb_indexShape3" style="position:absolute;left:-36px;top:-165px;width:802px;height:150px;z-index:12;">
+
+<ul class="drop_menu">
+<li><a href="./index.html" style="margin-left: 16px" title="Home">Home</a></li>
+<li><a href="./management.html" class="Menu" title="About">Management</a> </li>
+<li><a href="./faculity.html" class="Menu" title="Botany">Faculty</a> </li>
+<li><a href="./ssr.html" class="Menu" title="Botany">SSR Report</a> </li>
+<li><a href="./facility.html" class="Menu" title="Activities">Facility</a></li>
+<li><a href="./courses.html" class="Menu" title="Cources">Courses</a> </li>
+<li><a href="./Gallery.html" class="Menu" title="Research">Gallery</a> </li>
+<li><a href="./recruitment.html" class="Menu" title="Botany">Recruitment</a> </li>
+<li><a href="./about.html" class="Menu" title="Extension">About Us</a> </li>
+<li><a href="./contact.php" class="Menu" title="Photo Gallery">Contact Us</a> </li>
+
+</div>
+</div>
+</div>
+
+
+
+<div id="wb_Image3" style="position:absolute;left:195px;top:18px;width:115px;height:174px;z-index:12;" align="left">
+<img src="images/img0009.jpg" id="Image3" alt="" align="top" border="2" style="width:111px;height:170px;border:2px #316E72 solid;"></div>
+<div id="wb_Image1" style="position:absolute;left:475px;top:18px;width:112px;height:174px;z-index:13;" align="left">
+<img src="images/img0023.jpg" id="Image1" alt="" align="top" border="2" style="width:108px;height:170px;border:2px #316E72 solid;"></div>
+</div>
+
+
+
+
+
+
+<hr id="Line1" style="color:#000000;background-color:#000000;border:0px;margin:0;padding:0;position:absolute;left:937px;top:210px;width:0px;height:0px;z-index:18">
+
+
+
+</body>
+</html>
---
+++ wwb6.js
@@ -0,0 +1,219 @@
+function PlaySound(strFileName)
+{
+   var soundfile = eval("document." + strFileName);
+   try
+   {
+      soundfile.Play();
+   }
+   catch (e)
+   {
+      soundfile.DoPlay();
+   }
+}
+
+function SwapImage()
+{
+   var doc=document, args=arguments;
+   doc.$imgSwaps = new Array();
+   for(var i=2; i<args.length; i+=2)
+   {
+      var elem=FindObject(args[i]);
+      if(elem)
+      {
+         doc.$imgSwaps[doc.$imgSwaps.length]=elem;
+         elem.$src=elem.src;
+         elem.src=args[i+1];
+      }
+   }
+}
+
+function FindObject(id, doc)
+{
+   var child, elem;
+   if(!doc)
+      doc=document;
+   if(doc.getElementById)
+      elem=doc.getElementById(id);
+   else
+   if(doc.layers)
+      child=doc.layers;
+   else
+   if(doc.all)
+      elem=doc.all[id];
+   if(elem)
+      return elem;
+   if(doc.id==id || doc.name==id)
+      return doc;
+   if(doc.childNodes)
+      child=doc.childNodes;
+   if(child)
+   {
+      for(var i=0; i<child.length; i++)
+      {
+         elem=FindObject(id,child[i]);
+         if(elem)
+            return elem;
+      }
+   }
+   var frm=doc.forms;
+   if(frm)
+   {
+      for(var i=0; i<frm.length; i++)
+      {
+         var elems=frm[i].elements;
+         for(var j=0; j<elems.length; j++)
+         {
+            elem=FindObject(id,elems[i]);
+            if(elem) return elem;
+         }
+      }
+   }
+   return null;
+}
+
+function OnGoMenuFormLink(GoList)
+{
+   var url = GoList.options[GoList.selectedIndex].value;
+   var target = GoList.options[GoList.selectedIndex].className;
+   GoList.selectedIndex=0;
+   GoList.blur();
+   if (url)
+   {
+      NewWin=window.open(url,target);
+      window['NewWin'].focus()
+   }
+}
+
+function popupwnd(url, toolbar, menubar, locationbar, resize, scrollbars, statusbar, left, top, width, height)
+{
+   var popupwindow = this.open(url, '', 'toolbar=' + toolbar + ',menubar=' + menubar + ',location=' + locationbar + ',scrollbars=' + scrollbars + ',resizable=' + resize + ',status=' + statusbar + ',left=' + left + ',top=' + top + ',width=' + width + ',height=' + height);
+}
+
+function ShowObject(id, flag)
+{
+   var elem=FindObject(id);
+   if(elem)
+      elem.style.visibility=flag?'visible':'hidden';
+}
+
+function MoveObject(id, left, top)
+{
+   var elem=FindObject(id);
+   if(elem)
+   {
+      elem.style.left=left+'px';
+      elem.style.top=top+'px';
+   }
+}
+
+function SetImage(id, filename)
+{
+   var elem=FindObject(id);
+   if(elem)
+   {
+      elem.src=filename;
+   }
+}
+
+function SetStyle(id, className)
+{
+   var elem=FindObject(id);
+   if(elem)
+   {
+      elem.className=className;
+   }
+}
+
+function PlayAudio(id)
+{
+   var elem=FindObject(id);
+   if(elem)
+   {
+      elem.play();
+   }
+}
+
+function PauseAudio(id)
+{
+   var elem=FindObject(id);
+   if(elem)
+   {
+      elem.pause();
+   }
+}
+
+function StopAudio(id)
+{
+   var elem=FindObject(id);
+   if(elem)
+   {
+      elem.stop();
+   }
+}
+
+function ToggleExpand(id)
+{
+   var obj = FindObject(id);
+   if (!obj)
+   {
+      return false;
+   }
+   if (obj.style.display == "none")
+   {
+      obj.style.display = "";
+   }
+   else
+   {
+      obj.style.display = "none";
+   }
+   return false;
+}
+
+function FadeImage(id, duration)
+{
+   var millisec = Math.round(duration / 100);
+   var timer = 0;
+   for(i = 0; i <= 100; i++)
+   {
+      setTimeout("SetOpacity('" + id + "'," + i + ")",(timer * millisec));
+      timer++;
+   }
+}
+
+function SetOpacity(id, opacity)
+{
+   var element = document.getElementById(id).style;
+   element.opacity = (opacity / 100);
+   element.MozOpacity = (opacity / 100);
+   element.KhtmlOpacity = (opacity / 100);
+   element.filter = "alpha(opacity=" + opacity + ")";
+}
+
+function SlideImage(id, duration, direction, width)
+{
+   var millisec = Math.round(duration / 100);
+   var timer = 0;
+   if (direction == 0)
+   {
+      for(i = width; i >= 0; i--)
+      {
+         setTimeout("SetPosition('" + id + "'," + i + ")",(timer * millisec));
+         timer++;
+      }
+   }
+   else
+   {
+      for(i = -width; i <= 0; i++)
+      {
+         setTimeout("SetPosition('" + id + "'," + i + ")",(timer * millisec));
+         timer++;
+      }
+   }
+}
+
+function SetPosition(id, pos)
+{
+   var element = document.getElementById(id).style;
+   element.left = pos;
+}
+
